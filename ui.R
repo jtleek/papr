97185f0eb7acc2747d7d27e280694adb5ec92720
Nick Strayer <nstrayer@uvm.edu>
2017-02-25 15:51:31 -0600

    added a description to the PCA tab

--- ui.R
+++ ui.R
@@ -73,9 +73,30 @@ navbarPage(
                )
              ))
              )),
-  tabPanel("PCA Location",
+  tabPanel("What do I like?",
            fluidPage(
-             plotlyOutput("plotly"))),
+             div(id = "PCA_discussion",
+               h3("How we give you papers"),
+                 hr(),
+                 p("There are a lot of preprints out there. In an ideal world you could read them all, but you have things to do other than sit around an read abstracts all day."),
+                 p(
+                   span("In an attempt to help with this we have implemented what is known as a "),
+                   a(href = "https://en.wikipedia.org/wiki/Recommender_system","recomender engine."),
+                   span("What this allows us to do is tailor what abstracts we show you based upon your previous abstract rankings.")
+                   ),
+                 p(
+                   span("In more technical terms what we do is take every abstract in our database and record how many times different words occur. We then take this very large dimensional data (each abstract has a column for every unique word we saw in all of the abstracts), and use a technique known as"),
+                   a(href = "https://en.wikipedia.org/wiki/Principal_component_analysis", "Principle Components Analysis (PCA)"),
+                   span("on it to attempt to simplify these thousands of words down to a few key patterns.")
+                 ),
+                 p(
+                   span("Below is the raw data that we use to show you a given paper. Each blue dot represents one of the abstracts in our database plotted in the first three principle components. We start you at a random position in this cloud and when you like a paper we move your dot towards that given paper. The next abstract we select for you is then more likely to be drawn from the 'neighborhood' around your dot."),
+                   span("Please explore! See if you can notice trends in the cloud of abstracts. Does your position make sense to you in the context of it's surroundings? The more abstracts you rate the better our estimates of your tastes will be!")
+                 ),
+               plotlyOutput("plotly")
+             ) #end div
+            ) #end fluidpage
+          ),   #end tab
   tabPanel("About",
            fluidPage(includeMarkdown("./about.md"))),
   tabPanel("Help",
