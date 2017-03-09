896ed47a3d4bc4a32083d6e11ee15e1b18821565
Nick Strayer <nstrayer@uvm.edu>
2017-03-09 13:14:54 -0600

    Made the rate ui centered.

--- ui.R
+++ ui.R
@@ -29,57 +29,60 @@ navbarPage(
     ) #end fluidpage
   ),
   tabPanel("Rate",
-           sidebarLayout(
-             sidebarPanel(
-               tags$head(
-                 tags$script(src = "touchSwipe.js"),
-                 tags$script(src = "shinySwiper.js"),
-                 tags$link(rel = "stylesheet", type = "text/css", href = "appStyle.css")
-               ),
-               h3("Swipe abstract to rate the paper"),
-               hr(),
-               HTML(
-                  "<table style='line-height:1.5em;'>
-                    <tr>
-                      <td style='font-weight:normal;'><img src = 'images/swipe_right.png', style='height: 40px'></td>
-                      <td style='font-weight:normal;'> Exciting and Correct
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><img src = 'images/swipe_up.png', style='height: 40px'></td>
-                      <td> Exciting and Questionable
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><img src = 'images/swipe_down.png', style='height: 40px'></td>
-                      <td> Boring and Correct
-                      </td>
-                    </tr>
-                    <tr>
-                      <td><img src = 'images/swipe_left.png', style='height: 40px'></td>
-                      <td> Boring and Questionable
-                      </td>
-                    </tr>
-                  </table>"
-               ),
-               hr(),
-               h3("Rate papers & level up:"),
-               uiOutput("icon"),
-               em(textOutput("level")),
-               h3("Download your ratings:"),
-               downloadButton("download_data", "Download"),
-               h3("Tell someone about papr:"),
-               a(href = "https://twitter.com/intent/tweet?text=Check%20out%20papr%20its%20like%20Tinder%20for%20preprints%20https://jhubiostatistics.shinyapps.io/papr", icon("twitter")),
-               a(href = "https://www.facebook.com/sharer/sharer.php?u=https%3A//jhubiostatistics.shinyapps.io/papr", icon("facebook"))
-               ),
-             mainPanel(fluidPage(
-               div(id = "swipeCard", class = "card",  
-                   h3(id = "cardTitle", "Title"),
-                   hr(),
-                   p(id = "cardAbstract", "Abstract content")
+           div(id = "rate_wrapper",
+               sidebarLayout(
+                 sidebarPanel(width = 4, id = "rate_info",
+                              tags$head(
+                                tags$script(src = "touchSwipe.js"),
+                                tags$script(src = "shinySwiper.js"),
+                                tags$link(rel = "stylesheet", type = "text/css", href = "appStyle.css")
+                              ),
+                              h4("Swipe abstract to rate the paper"),
+                              hr(),
+                              HTML(
+                                "<table style='line-height:1.5em;'>
+                                <tr>
+                                <td style='font-weight:normal;'><img src = 'images/swipe_right.png', style='height: 40px'></td>
+                                <td style='font-weight:normal;'> Exciting and Correct
+                                </td>
+                                </tr>
+                                <tr>
+                                <td><img src = 'images/swipe_up.png', style='height: 40px'></td>
+                                <td> Exciting and Questionable
+                                </td>
+                                </tr>
+                                <tr>
+                                <td><img src = 'images/swipe_down.png', style='height: 40px'></td>
+                                <td> Boring and Correct
+                                </td>
+                                </tr>
+                                <tr>
+                                <td><img src = 'images/swipe_left.png', style='height: 40px'></td>
+                                <td> Boring and Questionable
+                                </td>
+                                </tr>
+                                </table>"
+                              ),
+                              hr(),
+                              h4("Rate papers & level up:"),
+                              uiOutput("icon"),
+                              em(textOutput("level")),
+                              h4("Download your ratings:"),
+                              downloadButton("download_data", "Download"),
+                              h4("Tell someone about papr:"),
+                              a(href = "https://twitter.com/intent/tweet?text=Check%20out%20papr%20its%20like%20Tinder%20for%20preprints%20https://jhubiostatistics.shinyapps.io/papr", icon("twitter")),
+                              a(href = "https://www.facebook.com/sharer/sharer.php?u=https%3A//jhubiostatistics.shinyapps.io/papr", icon("facebook"))
+                              ),
+                 mainPanel(fluidPage(
+                   div(id = "swipeCard", class = "card",  
+                       h3(id = "cardTitle", "Title"),
+                       hr(),
+                       p(id = "cardAbstract", "Abstract content")
+                   )
+                 ))
+                 )
                )
-             ))
-             )),
+          ),
   tabPanel("What do I like?",
            fluidPage(
              div(id = "PCA_discussion", class = "card",
